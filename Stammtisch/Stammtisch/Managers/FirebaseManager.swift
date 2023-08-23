73cb5138aadf379706321a67a0472d0e8a80aba5
Pascal Mönch <pascal@tbmoench.de>
2023-08-23 12:32:23 +0200

    Initial Commit

---
+++ Stammtisch/Stammtisch/Managers/FirebaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseManager.swift
+//  Stammtisch
+//
+//  Created by Pascal Mönch on 17.08.23.
+//
+
+import Foundation
---
+++ Stammtisch/Stammtisch/StammtischApp.swift
@@ -0,0 +1,17 @@
+//
+//  StammtischApp.swift
+//  Stammtisch
+//
+//  Created by Pascal Mönch on 28.07.23.
+//
+
+import SwiftUI
+
+@main
+struct StammtischApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
