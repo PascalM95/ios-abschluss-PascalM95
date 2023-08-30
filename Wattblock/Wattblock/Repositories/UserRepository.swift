9cb73c5ec5f2b4a523b43462eeeef1b0560709f8
Pascal Mönch <pascal@tbmoench.de>
2023-08-30 11:27:47 +0200

    - Edited Models, Enums, Managers & Repositories

--- Wattblock/Wattblock/Repositories/UserRepository.swift
+++ Wattblock/Wattblock/Repositories/UserRepository.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import FirebaseAuth
+import Combine
+
+class UserRepository {
+    
+    init() {
+        checkAuth()
+    }
+    
+    
+    
+    // MARK: - Variables
+    
+    static let shared = UserRepository()
+    
+    var user = CurrentValueSubject<FireUser?, Never>(nil)
+    var isUserLoggedIn = CurrentValueSubject<Bool, Never>(false)
+    
+    
+    
+    // MARK: - Functions
+    
+    private func checkAuth() {
+        
+    }
+    
+    func login() {
+        
+    }
+    
+    func register() {
+        
+    }
+    
+    func logout() {
+        
+    }
+    
+    private func createUser() {
+        
+    }
+    
+    private func fetchUser() {
+        
+    }
+}
