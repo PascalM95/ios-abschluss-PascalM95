6a0245203b21a58118eb83e54d0b13ddbb62caa7
Pascal Mönch <pascal@tbmoench.de>
2023-08-31 08:57:15 +0200

    - Edited Pflichtenheft

--- Wattblock/Wattblock/Views/StartView.swift
+++ Wattblock/Wattblock/Views/StartView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("background1")
+                .ignoresSafeArea()
+        }
     }
 }
 
