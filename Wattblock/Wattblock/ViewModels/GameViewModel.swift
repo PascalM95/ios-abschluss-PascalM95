e7c565955e804107a20ffb36c3ba4fd247a22a9b
Pascal Mönch <pascal@tbmoench.de>
2023-10-05 16:17:11 +0200

    - Edited GameViewModel

--- Wattblock/Wattblock/ViewModels/GameViewModel.swift
+++ Wattblock/Wattblock/ViewModels/GameViewModel.swift
@@ -29,66 +29,92 @@ class GameViewModel: ObservableObject {
     // MARK: - Functions
     
     func updateImage(_ totalScore: Int, _ newScore: Int) {
-        if totalScore == 2 && newScore == 2 {
+        
+        // MARK: - Logik 2 Punkte
+        if totalScore == 2 && newScore == 2 && !scores.contains(3) {
             imagePoints = Illustrations.twoPoints
-        } else if totalScore == 3 && newScore == 3 {
+            
+        //MARK: - Logik 3 Punkte
+        } else if totalScore == 3 && newScore == 3 && !scores.contains(2) {
             imagePoints = Illustrations.threePoints
-        } else if totalScore == 4 && (newScore == 2 || newScore == 4) {
+            
+        //MARK: - Logik 4 Punkte
+        } else if totalScore == 4 && (newScore == 2 || newScore == 4) && !scores.contains(3) {
             imagePoints = Illustrations.fourPoints
-        } else if totalScore == 5 && newScore == 2 {
-            imagePoints = Illustrations.fivePoints2
+        
+        //MARK: - Logik 5 Punkte
         } else if totalScore == 5 && (newScore == 3 || newScore == 5) {
             imagePoints = Illustrations.fivePoints1
-        } else if totalScore == 6 && (newScore == 2 || newScore == 4) {
+        } else if totalScore == 5 && newScore == 2 {
+            imagePoints = Illustrations.fivePoints2
+            
+        //MARK: - Logik 6 Punkte
+        } else if totalScore == 6 && (scores.first == 2 || scores.first == 4) && (newScore == 2 || newScore == 4) && !scores.contains(3) {
             imagePoints = Illustrations.sixPoints1
-        } else if totalScore == 6 && newScore == 3 {
+        } else if totalScore == 6 && scores.first == 3 && newScore == 3 && !scores.contains(2) {
             imagePoints = Illustrations.sixPoints2
-        } else if totalScore == 7 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 3 || newScore == 2 || newScore == 5) {
+            
+        //MARK: - Logik 7 Punkte
+        } else if totalScore == 7 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 3 || newScore == 2 || newScore == 5) && !scores.contains([3, 3]) {
             imagePoints = Illustrations.sevenPoints1
-        } else if totalScore == 7 && scores.first == 3 && (newScore == 2 || newScore == 4) {
+        } else if totalScore == 7 && scores.first == 3 && (newScore == 2 || newScore == 4) && !scores.contains([3, 3]) {
             imagePoints = Illustrations.sevenPoints2
-        } else if totalScore == 8 && (scores.first == 2 || scores.first == 4) && (newScore == 2 || newScore == 4) {
+           
+        //MARK: - Logik 8 Punkte
+        } else if totalScore == 8 && (scores.first == 2 || scores.first == 4) && (newScore == 2 || newScore == 4) && !scores.contains(3) {
             imagePoints = Illustrations.eightPoints1
-        } else if totalScore == 8 && (scores.first == 2 || scores.first == 5) && newScore == 3 {
+        } else if totalScore == 8 && (scores.first == 2 || scores.first == 5) && newScore == 3 && !scores.contains([2, 2]) {
             imagePoints = Illustrations.eightPoints2
-        } else if totalScore == 8 && scores.first == 3 && (newScore == 2 || newScore == 3 || newScore == 5) {
+        } else if totalScore == 8 && scores.first == 3 && (newScore == 2 || newScore == 3 || newScore == 5) && !scores.contains([2, 2]) {
             imagePoints = Illustrations.eightPoints3
-        } else if totalScore == 9 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) {
+         
+        //MARK: - Logik 9 Punkte
+        } else if totalScore == 9 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) && !scores.contains([3, 3]) {
             imagePoints = Illustrations.ninePoints1
-        } else if totalScore == 9 && scores.first == 3 && (newScore == 2 || newScore == 4) {
+        } else if totalScore == 9 && scores.first == 3 && (newScore == 2 || newScore == 4) && !scores.contains([3, 3]) {
             imagePoints = Illustrations.ninePoints2
-        } else if totalScore == 9 && scores.first == 3 && newScore == 3 {
+        } else if totalScore == 9 && scores.first == 3 && newScore == 3 && !scores.contains(2) {
             imagePoints = Illustrations.ninePoints3
-        } else if totalScore == 10 && (scores.first == 2 || scores.first == 4) && (newScore == 2 || newScore == 4) && !scores.contains([3, 3]) {
+           
+        //MARK: - Logik 10 Punkte
+        } else if totalScore == 10 && (scores.first == 2 || scores.first == 4) && (newScore == 2 || newScore == 4) && !scores.contains(3) {
             imagePoints = Illustrations.tenPoints1
-        } else if totalScore == 10 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 5) {
+        } else if totalScore == 10 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 5) && !scores.contains([2, 2, 2]) && !scores.contains([3, 3, 3]) {
             imagePoints = Illustrations.tenPoints2
-        } else if totalScore == 10 && scores.first == 3 && (newScore == 2 || newScore == 4 || newScore == 5) {
+        } else if totalScore == 10 && scores.first == 3 && (newScore == 2 || newScore == 4 || newScore == 5) && !scores.contains([2, 2, 2]) && !scores.contains([3, 3, 3]) {
             imagePoints = Illustrations.tenPoints3
-        } else if totalScore == 11 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) {
+         
+        //MARK: - Logik 11 Punkte
+        } else if totalScore == 11 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) && !scores.contains([3, 3]) {
             imagePoints = Illustrations.elevenPoints1
-        } else if totalScore == 11 && scores.first == 3 && (newScore == 2 || newScore == 4) {
+        } else if totalScore == 11 && scores.first == 3 && (newScore == 2 || newScore == 4) && !scores.contains([3, 3]) {
             imagePoints = Illustrations.elevenPoints2
-        } else if totalScore == 11 && (scores.first == 2 || scores.first == 5) && newScore == 3 {
+        } else if totalScore == 11 && (scores.first == 2 || scores.first == 5) && newScore == 3 && !scores.contains([2, 2]) {
             imagePoints = Illustrations.elevenPoints3
-        } else if totalScore == 11 && scores.first == 3 && (newScore == 3 || newScore == 5){
+        } else if totalScore == 11 && scores.first == 3 && (newScore == 2 || newScore == 3 || newScore == 5) && !scores.contains([2, 2]) {
             imagePoints = Illustrations.elevenPoints4
+            
+        //MARK: - Logik 12 Punkte
         } else if totalScore == 12 && (scores.first == 2 || scores.first == 4) && (newScore == 2 || newScore == 4) && !scores.contains(3) {
             imagePoints = Illustrations.twelvePoints1
         } else if totalScore == 12 && scores.first == 3 && newScore == 3 && !scores.contains(2) {
             imagePoints = Illustrations.twelvePoints2
-        } else if totalScore == 12 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) {
+        } else if totalScore == 12 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) && !scores.contains([3, 3, 3]) {
             imagePoints = Illustrations.twelvePoints3
-        } else if totalScore == 12 && scores.first == 3 && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) {
+        } else if totalScore == 12 && scores.first == 3 && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) && !scores.contains([3, 3, 3]) {
             imagePoints = Illustrations.twelvePoints4
-        } else if totalScore == 13 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) {
+            
+        //MARK: - Logik 13 Punkte
+        } else if totalScore == 13 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) && !scores.contains([3, 3]) {
             imagePoints = Illustrations.thirteenPoints1
-        } else if totalScore == 13 && scores.first == 3 && (newScore == 2 || newScore == 4) {
+        } else if totalScore == 13 && scores.first == 3 && (newScore == 2 || newScore == 4) && !scores.contains([3, 3]) {
             imagePoints = Illustrations.thirteenPoints2
-        } else if totalScore == 13 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 5) {
+        } else if totalScore == 13 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 5) && !scores.contains([2, 2, 2]) {
             imagePoints = Illustrations.thirteenPoints3
-        } else if totalScore == 13 && scores.first == 3 && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) {
+        } else if totalScore == 13 && scores.first == 3 && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) && !scores.contains([2, 2, 2]) {
             imagePoints = Illustrations.thirteenPoints4
+            
+        //MARK: - Logik 14 Punkte
         } else if totalScore == 14 && (scores.first == 2 || scores.first == 4) && (newScore == 2 || newScore == 4) && !scores.contains(3) {
             imagePoints = Illustrations.fourteenPoints1
         } else if totalScore == 14 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) && !scores.contains([3, 3, 3]) {
@@ -99,6 +125,8 @@ class GameViewModel: ObservableObject {
             imagePoints = Illustrations.fourteenPoints4
         } else if totalScore == 14 && scores.first == 3 && (newScore == 2 || newScore == 3 || newScore == 5) && !scores.contains([2, 2]) {
             imagePoints = Illustrations.fourteenPoints5
+            
+        //MARK: - Logik 15 Punkte
         } else if totalScore == 15 && scores.first == 3 && newScore == 3 && !scores.contains(2) {
             imagePoints = Illustrations.fifteenPoints1
         } else if totalScore == 15 && (scores.first == 2 || scores.first == 4 || scores.first == 5) && (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5) && !scores.contains([2, 2, 2, 2]) && !scores.contains([3, 3, 3, 3]) {
@@ -119,6 +147,9 @@ class GameViewModel: ObservableObject {
     func removeScore(_ score: Int, from scores: inout [Int]) {
         if let index = scores.firstIndex(of: score) {
             scores.remove(at: index)
+        }
+        if scores.isEmpty {
+            imagePoints = Image("")
         }
     }
     
